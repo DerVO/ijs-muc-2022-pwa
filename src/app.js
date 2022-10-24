377e23cabb860a7641c64830688de36cf1a9e0d1
Björn Gießler <bgiessler@kaletsch-medien.de>
2022-10-24 09:58:14 +0200

    Lab#4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import {bresenhamLine, getImage, toBlob} from "./helpers.js";
 
 const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d', {
+  desynchronized: true
+})
 
 ctx.fillStyle = 'white'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 ctx.fillStyle = 'black'
+
+canvas.addEventListener('pointerdown', event => {})
+canvas.addEventListener('pointermove', event => {
+  ctx.fillRect(event.offsetX, event.offsetY, 2, 2)
+})
+canvas.addEventListener('pointerup', () => {})
